8f3fc29e29c294f6566f1f99123c9db3b2602ed3
Chris Ashman <chrisashman96@gmail.com>
2020-01-30 21:08:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 def Dog 
   def name=(dog_name)
-    @this_dogs 
+    @this_dogs_name = dog_name
+  end
+  
+  def name 
+    @this_dogs_name
+  end
+   
+   def bark 
+     puts "Woof!"
+   end
+  end
+  
+  fido = Dog.new 
+  fido.name = "Fido"
+  
+  fido.name
+  fido.bark
